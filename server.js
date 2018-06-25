ba893b4824ce3e4286a6c8df7f83f02ac4b40742
Le Xuan Quynh <lexuanquynh@secure.janeto.com>
2018-06-25 17:30:34 +0700

    fix build

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 server.listen(3000, () => {
   console.log('JSON Server is running')
 });
 
-
 //Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
-});
-
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080, ()=> {
-    console.log('server is runing on', 8080);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server started in port: ${PORT}`);
 });
