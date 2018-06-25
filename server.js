e8f80d56bf36c5cdc0de11d81ed75cd02b6179be
Le Xuan Quynh <lexuanquynh@secure.janeto.com>
2018-06-25 17:53:36 +0700

    fix build

--- server.js
+++ server.js
@@ -11,11 +11,12 @@
 
 //Install express server
 const express = require('express');
-const app = express();
+var jsonServer = require('json-server');
 
+const app = express();
+app.use('/api', jsonServer.router('db.json'));
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-
 // Start the app by listening on the default Heroku port
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
